a9ed4374166f7e79b760a41e7a5a96219262467f
sudarshan kshetri <sudarshandn1999@gmail.com>
2022-05-02 13:49:42 +0545

    data maping

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,11 +1,17 @@
 import Navbar from "./components/Nabbar";
 import Hero from "./components/Hero";
-
+import data from "./data";
+import Card from "./components/Card";
+import "./style.css";
 function App() {
+  const cardArray = data.map((dat) => {
+    return <Card item={dat} />;
+  });
   return (
     <div>
       <Navbar />
       <Hero />
+      <div class="card-list">{cardArray}</div>
     </div>
   );
 }
