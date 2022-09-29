8d671eef627b1d247b696e8c677729fcf6212fd0
sudarshan kshetri <sudarshandn1999@gmail.com>
2022-09-29 13:01:17 +0545

    form

--- ReactCourse/react-advanced-2020-master/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ ReactCourse/react-advanced-2020-master/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -4,20 +4,66 @@ import React, { useState } from "react";
 // const inputValue = input.value
 // React
 // value, onChange
+// dynamic object keys
 
 const ControlledInputs = () => {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit button");
+    if (firstName && email) {
+      const person = { id: new Date().getTime().toString(), firstName, email };
+      setPeople((people) => {
+        return [...people, person];
+      });
+      setFirstName("");
+      setEmail("");
+    } else {
+      console.log("empty values");
+    }
+  };
   return (
     <>
-      <form className="form">
-        <div className="form-control">
-          <label htmlFor="firstName">Name : </label>
-          <input type="text" id="firstName" name="firstName" />
-        </div>
-        <div className="form-control">
-          <label htmlFor="emailName">Name : </label>
-          <input type="email" id="emailName" name="firstName" />
-        </div>
-      </form>
+      <article>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="firstName">Name : </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email : </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <button type="submit">add person</button>
+        </form>
+
+        {console.log(people)}
+
+        {people.map((person, index) => {
+          const { id, firstName, email } = person;
+          return (
+            <div className="item" key={id}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
+      </article>
     </>
   );
 };
