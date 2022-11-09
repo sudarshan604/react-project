745e7e5ea63980a05da6883a10ca41c77d0cd156
sudarshan kshetri <sudarshandn1999@gmail.com>
2022-11-09 23:11:11 +0545

    adding remove button

--- ecommerce/cart.js
+++ ecommerce/cart.js
@@ -6,20 +6,25 @@ const cartBox = document.querySelector("[data-cart-single-item]");
 const dataItemCount = document.querySelector("[data-item-count]");
 const cartWrapper = document.querySelector("[data-box-cart ]");
 
-const shoppingCart = [];
+const cartClose = document.querySelector("[data-cart-close]");
+
+let shoppingCart = [];
 
 export function addCart(id) {
+  renderShoppingCart(id);
+  hideCart();
+  removeCartItem();
+}
+
+function renderShoppingCart(id) {
   const existingId = shoppingCart.find((item) => item.id == id);
   if (existingId) {
     existingId.quantity++;
-    console.log(existingId.quantity);
   } else {
     shoppingCart.push({ id: id, quantity: 1 });
   }
   cartWholeBox.innerHTML = " ";
   shoppingCart.forEach(renderCart);
-  dataItemCount.innerText = shoppingCart.length;
-  hideCart();
 }
 
 function renderCart(data) {
@@ -30,6 +35,9 @@ function renderCart(data) {
   const cartImg = newAddCartItem.querySelector("img");
 
   cartImg.src = slectCartIem.image;
+
+  const closeId = newAddCartItem.querySelector("[data-close-cart-id]");
+  closeId.dataset.closeId = data.id;
 
   const objectQuantity = newAddCartItem.querySelector("[data-item-quantity]");
 
@@ -42,9 +50,30 @@ function renderCart(data) {
 }
 
 export function hideCart() {
-  if (shoppingCart.length == 0) {
-    console.log("hello");
+  dataItemCount.innerText = shoppingCart.length;
 
+  if (shoppingCart.length == 0) {
     cartWrapper.classList.add("hidden");
   }
+}
+
+function removeCartItem() {
+  document.addEventListener("click", (e) => {
+    if (!e.target.matches("[data-cart-close]")) return;
+
+    const itemIds = e.target.closest("[data-close-cart-id]").dataset.closeId;
+    console.log("hello");
+    removeCartList(parseInt(itemIds));
+  });
+}
+
+function removeCartList(id) {
+  const existinCard = shoppingCart.find((entry) => entry.id === id);
+  if (existinCard === null) return;
+
+  shoppingCart = shoppingCart.filter((entry) => entry.id !== id);
+
+  cartWholeBox.innerHTML = " ";
+  hideCart();
+  shoppingCart.forEach(renderCart);
 }
